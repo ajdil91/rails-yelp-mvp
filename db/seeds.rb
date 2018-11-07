f17eb47270c8f798e47ce2dbc766d221ceb98eaf
Amanda Dilworth <amandadilworth@yahoo.com>
2018-11-07 18:31:30 +0900

    used rails to created app with nested resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Ootoya',
+    address:      'Meguro',
+    phone_number: '03-1111-2222',
+    category: 'japanese'
+  },
+  {
+    name:         'Little Mermaid',
+    address:      'Ebisu',
+    phone_number:  '03-1111-3333',
+    category: 'french'
+  },
+  {
+    name:         'Belgian Beer',
+    address:      'Shibuya',
+    phone_number:  '03-1111-4444',
+    category: 'belgian'
+  },
+  {
+    name:         'Pizza-la',
+    address:      'Naka-meguro',
+    phone_number:  '03-1111-3334',
+    category: 'italian'
+  },
+  {
+    name:         'Dim Sum',
+    address:      'Meguro',
+    phone_number:  '03-1111-5555',
+    category: 'chinese'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
